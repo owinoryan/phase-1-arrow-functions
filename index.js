093750d2b3542b28aa1f7db466eac797d8d78691
ryanowino <owinoryan@gmail.com>
2024-07-01 10:29:34 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const divide = (a, b) => a / b;
+
+let a = 2000;
+let b = 100;
+console.log(divide(a, b));
+
+const square = (x) => x*x;
+let x = 2   
+
+console.log(square(x));
+
+{const add = () => a + b;
+ let a = 3
+ let b = 4
+
+ console.log(add(a, b))}
